712f8b23b580f4445700f8f7c45f3f32d74fe486
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-26 16:58:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray(array, index){
   return (array[index]);
 }
 
-function destructiveRemoveElementFromBeginnningOfArray(array, element){
+function destructivelyRemoveElementFromBeginnningOfArray(array, element){
   array.shift(element);
   return array; 
 }
