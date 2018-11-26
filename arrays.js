d29e7f8b9af5b39824ed728e839d6b1b31d15c31
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-26 16:28:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,37 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  
+}
+
+function addElementToEndOfArray(array, element){
+  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  
+}
+
+function accessElementInArray(array, index){
+  
+}
+
+function destructiveRemoveElementFromBeginnningOfArray(array, element){
+  
+}
+
+function removeElementFromBeginningOfArray(array, element){
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray(array, element){
+  
+}
+
+function removeElementFromEndOfArray(array, element){
+  
+}
