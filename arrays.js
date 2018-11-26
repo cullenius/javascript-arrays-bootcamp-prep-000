f340a518e63b9f9419f7d6d5a86fa6fbf5773225
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-26 16:44:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  
+  document.array([index])
 }
 
 function destructiveRemoveElementFromBeginnningOfArray(array, element){
