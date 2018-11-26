f2b55df1b477ef758b63717772084bef3839b02a
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-26 16:33:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift()
+  array.unshift(array, element)
 }
 
 function addElementToEndOfArray(array, element){
